5ff892ec5c6e1de8824f8a786155c5060f5aef7a
Nephilim24 <muratacc21@gmail.com>
2025-04-12 20:53:27 +0500

    top 10 section

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,8 +1,16 @@
-var swiper = new Swiper(".mySwiper", {
+const swiper = new Swiper(".mySwiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   slidesPerView: 5.5,
+  spaceBetween: 24,
+});
+const topSwiper = new Swiper(".top__swiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 3.2,
   spaceBetween: 24,
 });
