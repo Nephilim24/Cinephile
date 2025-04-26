27712ca88013b92a9764cd9d12d0b70e29b641c5
Nephilim24 <muratacc21@gmail.com>
2025-04-26 21:01:54 +0500

    almost fully made header bg and next btn script

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,3 +1,39 @@
+// async function getNowMovies() {
+//   try {
+//     
+//     
+//     let currentIndex = 0;
+
+//     function update() {
+//       header.append(generateBg(currentIndex, nowMovies, imgPath));
+//       header.querySelector('h1').innerHTML = nowMovies.results[currentIndex].title;
+//       header.querySelector('p').innerHTML = nowMovies.results[currentIndex].overview;
+//       let nextIndex = (currentIndex + 1) % nowMovies.results.length;
+//       const headerNextBtn = header.querySelector('.header__next');
+//       headerNextBtn.querySelector('h3').innerHTML = nowMovies.results[nextIndex].title;
+//       headerNextBtn.append(generateBg(nextIndex, nowMovies, imgPathMini));
+//       currentIndex = (currentIndex + 1) % nowMovies.results.length;
+//     }
+//     update();
+//     setInterval(update, 10000);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+
+// function generateBg(index, nowMovies, imgPath) {
+//   const headerBg = document.createElement('img');
+//   headerBg.setAttribute('src', `${imgPath}${nowMovies.results[index].backdrop_path}`);
+//   headerBg.classList.add('show');
+
+
+//   return headerBg;
+// }
+
+// getNowMovies();
+
+
 const options = {
   method: 'GET',
   headers: {
@@ -7,56 +43,61 @@ const options = {
 };
 
 const header = document.querySelector('.header');
-const headerNextContent = document.querySelector('.header__next-content');
-const imgPath = 'https://image.tmdb.org/t/p/original/'
-const imgPathNext = 'https://image.tmdb.org/t/p/w500/'
+const imgPath = 'https://image.tmdb.org/t/p/original/';
+const imgPathMini = 'https://image.tmdb.org/t/p/w500/';
 
-async function getNowMovies() {
+let currentIndex = 0;
+let nowMovies;
+let headerBg, nextBg, titleElem, descrElem, headerNextButton;
 
+
+async function getNowMoviews() {
   try {
-    const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=ru-RU&page=1', options);
-    const nowMovies = await response.json();
+    const response =
+      await fetch('https://api.themoviedb.org/3/movie/now_playing?language=ru-RU&page=1', options);
 
-    let currentIndex = 0;
-
-    function update() {
-      const headerBg = document.createElement('img');
-
-      headerBg.setAttribute('src', `${imgPath}${nowMovies.results[currentIndex].backdrop_path}`);
-      headerBg.classList.add('show');
-      header.append(headerBg);
-      header.querySelector('h1').innerHTML = nowMovies.results[currentIndex].title;
-      header.querySelector('p').innerHTML = nowMovies.results[currentIndex].overview;
-      const headerNextContentBg = headerNextContent.querySelector('img');
-      // headerNextContentBg.setAttribute('src', `${imgPathNext}${nowMovies.results[currentIndex + 1].backdrop_path}`);
-      headerNextContent.querySelector('h3').innerHTML = nowMovies.results[(currentIndex + 1) % nowMovies.results.length].title;
-
-      const oldImage = header.querySelector('img');
-      if (oldImage) {
-        oldImage.classList.remove('show');
-        setTimeout(() => oldImage.remove(), 1000);
-      }
-
-      const nextButton = header.querySelector('.header__next');
-      nextButton.addEventListener("click", () => {
-        update();
-        if (interval > 0) {
-          clearInterval(interval);
-        } else {
-          setInterval(update, 5000);
-        }
-      })
-
-      currentIndex = (currentIndex + 1) % nowMovies.results.length;
-    }
+    nowMovies = await response.json();
+    init();
     update();
-
-    let interval = setInterval(update, 5000);
-
+    setInterval(update, 5000);
   } catch (error) {
     console.error(error);
   }
+}
+
+
+function init() {
+  headerBg = document.createElement('img');
+  headerBg.classList.add('show');
+  header.append(headerBg);
+  // 
+  titleElem = document.createElement('h1');
+  descrElem = document.createElement('p');
+  header.querySelector('.header__content').append(titleElem, descrElem);
+  // 
+  headerNextButton = header.querySelector('.header__next');
+  //
+  nextBg = document.createElement('img');
+  nextBg.classList.add('show');
+  headerNextButton.append(nextBg);
+  headerNextButton.addEventListener('click', update);
+}
+
+function update() {
+  const nextIndex = (currentIndex + 1) % nowMovies.results.length;
+
+  headerBg.src = `${imgPath}${nowMovies.results[currentIndex].backdrop_path}`;
+  headerBg.classList.add('show');
+  setTimeout(() => headerBg.classList.remove('show'), 500);
+
+  titleElem.innerHTML = nowMovies.results[currentIndex].title;
+  descrElem.innerHTML = nowMovies.results[currentIndex].overview;
+
+  nextBg.src = `${imgPath}${nowMovies.results[nextIndex].backdrop_path}`;
+  headerNextButton.querySelector('h3').innerHTML = nowMovies.results[nextIndex].title;
+
+  currentIndex = nextIndex;
 
 }
 
-getNowMovies();
+getNowMoviews();
